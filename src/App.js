1de0704afeaeb5ae6666bf8187e1c0231ad3b29f
vempalla1717 <vempallasuneel24@gmail.com>
2024-02-28 10:17:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+this is a react project   
+<h1>This is suneelproject</h1>
+     </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
